65ab18029d0fd64e5e96459471df67771908f081
noumanhafeez <noumanhafeez095@gmail.com>
2025-01-28 09:24:22 +0500

    proper integeration of icons in android

--- android/app/src/main/java/com/code_task/MainApplication.kt
+++ android/app/src/main/java/com/code_task/MainApplication.kt
@@ -1,5 +1,6 @@
 package com.code_task
 
+import com.oblador.vectoricons.VectorIconsPackage;
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
@@ -20,6 +21,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              add(VectorIconsPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
